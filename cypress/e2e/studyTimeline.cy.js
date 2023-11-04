05e6c42af5937fa1d6ba8f4011913e5f2eb01b56
c1b3rt00lk1t <c1b3rt00lk1t@protonmail.com>
2023-11-04 23:39:57 +0100

    Test displays the footer

--- cypress/e2e/studyTimeline.cy.js
+++ cypress/e2e/studyTimeline.cy.js
@@ -1,5 +1,13 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("template spec", () => {
+  // const URL = "https://studytimeline-c92b5.web.app";
+  const URL = "http://localhost:5000";
+
+  beforeEach(() => {
+    cy.viewport(1600, 900);
+    cy.visit(URL);
+  });
+
+  it("displays the footer", () => {
+    cy.contains("developed by");
+  });
+});
